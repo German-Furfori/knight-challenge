429326dc94f6ee22cc31234899ba9759d0f613a4
Germán Furfori <german.furfori@paygoal.com.ar>
2025-07-10 16:38:23 +0200

    Out of the board and invalid start position tests

--- src/test/java/org/jobrapido/challenge/service/KnightServiceTest.java
+++ src/test/java/org/jobrapido/challenge/service/KnightServiceTest.java
@@ -2,12 +2,19 @@ package org.jobrapido.challenge.service;
 
 import org.jobrapido.challenge.MainTest;
 import org.jobrapido.challenge.dto.output.ResultDto;
+import org.jobrapido.challenge.exception.InvalidStartPositionException;
+import org.jobrapido.challenge.exception.OutOfTheBoardException;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
 import org.mockito.Mockito;
 import java.util.List;
 import java.util.Set;
+import java.util.stream.Stream;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 public class KnightServiceTest extends MainTest {
 
@@ -18,8 +25,27 @@ public class KnightServiceTest extends MainTest {
 
     KnightService knightService = new KnightService(boardService, commandsService);
 
+    private static Stream<Arguments> multipleInvalidStartPositions() {
+        return Stream.of(
+                Arguments.of(List.of("START -1,0,NORTH", "MOVE 4")),
+                Arguments.of(List.of("START 0,-1,NORTH", "MOVE 4")),
+                Arguments.of(List.of("START 5,0,NORTH", "MOVE 4")),
+                Arguments.of(List.of("START 0,5,NORTH", "MOVE 4")),
+                Arguments.of(List.of("START 2,2,NORTH", "MOVE 4"))
+        );
+    }
+
+    private static Stream<Arguments> multipleOutOfBoardMovements() {
+        return Stream.of(
+                Arguments.of(List.of("START 1,1,NORTH", "MOVE 10")),
+                Arguments.of(List.of("START 1,1,NORTH", "ROTATE SOUTH", "MOVE 10")),
+                Arguments.of(List.of("START 1,1,NORTH", "ROTATE EAST", "MOVE 10")),
+                Arguments.of(List.of("START 1,1,NORTH", "ROTATE WEST", "MOVE 10"))
+        );
+    }
+
     @Test
-    void executeKnightMovements_withoutGettingObstacles_expectCorrectOutput() {
+    void executeKnightMovements_withoutGettingObstacles_expectSuccessOutput() {
         Mockito.when(boardService.getBoard()).thenReturn(this.getBoard(
                 5, 5, Set.of("20", "21", "22", "23")
         ));
@@ -41,7 +67,7 @@ public class KnightServiceTest extends MainTest {
     }
 
     @Test
-    void executeKnightMovements_gettingObstacles_expectCorrectOutput() {
+    void executeKnightMovements_gettingObstacles_expectSuccessOutput() {
         Mockito.when(boardService.getBoard()).thenReturn(this.getBoard(
                 10, 10,
                 Set.of("08", "18", "17", "16", "15", "14", "22", "32", "42", "54",
@@ -70,6 +96,32 @@ public class KnightServiceTest extends MainTest {
         assertEquals(3, result.getPosition().getY());
         assertEquals("NORTH", result.getPosition().getDirection());
         assertEquals("SUCCESS", result.getStatus());
+    }
+
+    @ParameterizedTest
+    @MethodSource("multipleInvalidStartPositions")
+    void executeKnightMovements_withMultipleInvalidStartPositions_expectInvalidStartPositionOutput(List<String> commands) {
+        Mockito.when(boardService.getBoard()).thenReturn(this.getBoard(5, 5, Set.of("22")));
+        Mockito.when(commandsService.getCommands()).thenReturn(this.getCommands(commands));
+
+        InvalidStartPositionException ex = assertThrows(InvalidStartPositionException.class, () -> {
+            this.knightService.moveKnight();
+        });
+
+        assertEquals("INVALID_START_POSITION", ex.getMessage());
+    }
+
+    @ParameterizedTest
+    @MethodSource("multipleOutOfBoardMovements")
+    void executeKnightMovements_withMultipleOutOfBoardMovements_expectOutOfTheBoardOutput(List<String> commands) {
+        Mockito.when(boardService.getBoard()).thenReturn(this.getBoard(5, 5, Set.of()));
+        Mockito.when(commandsService.getCommands()).thenReturn(this.getCommands(commands));
+
+        OutOfTheBoardException ex = assertThrows(OutOfTheBoardException.class, () -> {
+            this.knightService.moveKnight();
+        });
+
+        assertEquals("OUT_OF_THE_BOARD", ex.getMessage());
     }
 
 }
